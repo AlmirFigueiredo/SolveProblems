18fe85ea5125ee502874de9c51ee2568a7ae1f7c
Jose Almir <joseamfj@outlook.com>
2023-03-21 20:06:55 -0300

    Solve libraryFine using java

--- UnicapMarathon/LibraryFine/Solution.java
+++ UnicapMarathon/LibraryFine/Solution.java
@@ -1,5 +1,25 @@
 package UnicapMarathon.LibraryFine;
 
+import java.util.Scanner;
+
 public class Solution {
-    
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int d = input.nextInt();
+        int m = input.nextInt();
+        int y = input.nextInt();
+        int d2 = input.nextInt();
+        int m2 = input.nextInt();
+        int y2 = input.nextInt();
+        if(d <= d2 && m <= m2 && y <= y2 || (y<y2) || (y==y2 && m<m2)) {
+            System.out.println(0);
+        } else if(y==y2 && m2==m) {
+            System.out.println(15*(d-d2));
+        } else if(y==y2) {
+            System.out.println(500*(m-m2));
+        } else {
+            System.out.println(10000);
+        }
+        input.close();
+    }
 }
