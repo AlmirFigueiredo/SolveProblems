501b711138b38f16c654e91865342c94cc69aa94
Jose Almir <joseamfj@outlook.com>
2023-03-21 21:01:56 -0300

    Solve comparetriplets using java

--- UnicapMarathon/CompareTheTriplets/Solution.java
+++ UnicapMarathon/CompareTheTriplets/Solution.java
@@ -5,8 +5,27 @@ import java.util.Scanner;
 public class Solution {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        
-
+        int a[] = new int[3];
+        int b[] = new int[3];
+        for(int i = 0; i < 6; i++) {
+            if( i < 3) {
+                a[i] = input.nextInt();
+            } else {
+                b[i%3] = input.nextInt();
+            }
+        }
+        int resultA = 0;
+        int resultB = 0;
+        for(int i = 0; i < 3; i++) {
+            if(a[i] > b[i]) {
+                resultA += 1;
+            } else if(a[i] < b[i]) {
+                resultB += 1;
+            } else {
+                continue;
+            }   
+        }
+        System.out.printf("%d %d\n", resultA, resultB);
         input.close();
     }
 }
